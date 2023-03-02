3e2c6e3b2a181ab0079501cbd272305c6f36699a
Jordan Wolter <jwolter456@gmail.com>
2023-03-01 19:43:32 -0500

    tweaked spacing in project section

--- portfolio-two/src/components/Home.jsx
+++ portfolio-two/src/components/Home.jsx
@@ -22,6 +22,9 @@ export default function Home () {
                     </div>
                 </div>
             </div>
+            <div className='projectDiv'>
+                <h1 className='projectTitle'>Projects</h1>
+            </div>
             <div className="sectionOne">
                 <div className="leftSection">
                     <div className="leftContent">
@@ -69,71 +72,6 @@ export default function Home () {
                 
             </div>    
 
-
-
-
-            
-            {/* <div className="sectionTwo">
-                <div className="leftSectionTwo">
-                    <div className="leftContentTwo">
-                    <h1 className="leftSectionHeaderTwo">LOREM IPSUM</h1>
-                    <a href='https://www.roostergrin.com/#case-studies' target='_blank' rel="noopener noreferrer"><button className="leftSectionButtonTwo">BUTTON BUTTON</button></a>
-                    </div>
-                </div>
-                <div className="rightSectionTwo">
-                </div>
-            </div>
-            <div className="sectionThree">
-                <div className="leftSectionThree">
-                    <div className="leftContentThree">
-                        <h1 className="leftSectionHeaderThree">LOREM IPSUM <br></br> DOLOR SIT AMET</h1>
-                        <p className="leftSectionBodyThree">Lorem ipsum dolor sit amet, et felis intger. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quique risus vitae semper duis feugiat.</p>
-                        <a className='leftSectionButtonThree' href="https://www.roostergrin.com/web" target="_blank" rel="noopener noreferrer"> &gt; Lorem Ipsum</a>
-                    </div>
-                </div>
-                <div className="rightSectionThree">
-                        <div className="item1">
-                            <div className="svg1"></div>
-                            <div className="text1">LOREM</div>
-                        </div>
-                        <div className="item2">
-                            <div className="svg2"></div>
-                            <div className="text2">LOREM IPSUM</div>
-                        </div>
-                        <div className="item3">
-                            <div className="svg3"></div>
-                            <div className="text3">LOREM IPSUM</div>
-                        </div>
-                        <div className="item4">
-                            <div className="svg4"></div>
-                            <div className="text4">LOREM</div>
-                        </div>
-                </div>
-            </div>
-            <div className="sectionFour">
-                <div className="leftSectionFour">
-                </div>
-                <div className="rightSectionFour">
-                    <div className="rightContentFour">
-                        <h1 className="rightSectionHeaderFour">LOREM IPSUM</h1>
-                        <p className="rightSectionBodyFour">Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula.</p>
-                        <a href='https://www.roostergrin.com/thread' target='_blank' rel="noopener noreferrer"><button className="rightSectionButtonFour">BUTTON BUTTON</button></a>
-                    </div>
-                </div>
-            </div>
-            <div className="sectionFive">
-                <div className="leftSectionFive">
-                    <div className="leftContentFive">
-                        <h1 className="leftSectionHeaderFive">LOREM IPSUM <br></br>DOLOR SIT AMET</h1>
-                        <div className='reactStars'>
-                        </div>
-                        
-                    </div>
-                </div>
-                <div className="rightSectionFive">
-                        <Carousel />
-                </div>
-            </div>  */}
             <div className='footer'>
                 <div className='footerContent'>
                     <h4>Follow Us:</h4>
